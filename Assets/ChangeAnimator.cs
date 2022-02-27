50fec582fbb0950c36b85e3b73aaa8e3f33ff99e
yeentuan <sataporng@gmail.com>
2022-02-27 22:14:00 +0700

    first commit

---
+++ Assets/ChangeAnimator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+   public void getAnimatorCatwalk(){
+       this.GetComponent<Animator>().Play("Catwalk Walk Turn 180 Tight");
+   }
+   public void getAnimatorHiphop(){
+       this.GetComponent<Animator>().Play("Hip Hop Dancing");
+   }
+   public void getAnimatorSilly(){
+       this.GetComponent<Animator>().Play("Silly Dancing");
+   }
+}
---
+++ Assets/ChangeColors.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColors : MonoBehaviour
+{
+    
+         // Start is called before the first frame update
+         public void getRed(){
+             this.GetComponent<Renderer>().material.color = Color.red;
+
+         }
+        public void getBlue(){
+             this.GetComponent<Renderer>().material.color = Color.blue;
+
+         }
+         public void getGreen(){
+             this.GetComponent<Renderer>().material.color = Color.green;
+
+         }
+}
